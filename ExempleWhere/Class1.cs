fe3cf09af24805225d869145deb3376fb54d3ef3
davi321522@gmail.com <davi321522@gmail.com>
2024-06-25 15:48:51 -0300

    testando o WHERE

--- ExempleWhere/Class1.cs
+++ ExempleWhere/Class1.cs
@@ -10,20 +10,23 @@ namespace ExempleWhere
     {
 
 
-        
+
 
 
         public void Metodic()
         {
             List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //usamos o método Where na lista de numeros e passamos uma função lambda onde a condi
+            //ção é o resto ser igual a 0, sendo assim somente os numeros armazenados em numerosPares
+            //serão pares por conta de nosso filtro where
             List<int> numerosPares = new List<int>(numeros.Where(numero => numero % 2 == 0));
 
-            
+            int numb = 4;
 
             foreach (int num in numerosPares)
             {
                 Console.WriteLine(num);
-            }
+            };
 
 
             
