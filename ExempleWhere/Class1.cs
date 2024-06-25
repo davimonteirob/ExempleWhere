ebc22058d6c3414544ff404d1dd7f4b01620dc17
davi321522@gmail.com <davi321522@gmail.com>
2024-06-25 15:16:26 -0300

    testando o LINQ com WHERE

---
+++ ExempleWhere/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleWhere
+{
+    internal class Class1
+    {
+
+
+        
+
+
+        public void Metodic()
+        {
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> numerosPares = new List<int>(numeros.Where(numero => numero % 2 == 0));
+
+            
+
+            foreach (int num in numerosPares)
+            {
+                Console.WriteLine(num);
+            }
+
+
+            
+        }
+    }
+}
